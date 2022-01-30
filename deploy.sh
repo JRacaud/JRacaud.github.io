c17b669349f4de0ab7114c781a10b419cdfabfde
Jonathan Racaud <jonathan.racaud@outlook.com>
2022-01-30 14:47:34 +0100

    Added a deploy.sh script and removed github actions

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+read -p "Commit message: " message
+
+# Remove the previous version of the website
+rm -rf ./public
+
+# Generate the website
+hugo --minify
+
+# Copy the files into a temp directory
+mkdir .deployed
+cp -r ./public/* .deployed
+
+# Checkout gh-pages branch and update everything
+git checkout gh-pages
+rm -rf *
+
+mv ./deployed/* .
+
+git commit -am $message
+git push
+
+# We can now go back to the main branch and delete the temp folder
+git checkout main
+rm -rf .deployed
