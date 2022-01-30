6c03a7b7c861299430052ff2dd6136ade04aebf8
Jonathan Racaud <jonathan.racaud@outlook.com>
2022-01-30 15:46:34 +0100

    Changed how the deploy script works

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
+ROOT=$PWD
+PUBLIC_FOLDER=$ROOT/public
+DEPLOY_FOLDER=.deploy
+
 read -p "Commit message: " message
 
-# Remove the previous version of the website
-rm -rf ./public
-
-# Generate the website
+rm -rf $PUBLIC_FOLDER
 hugo --minify
 
 if [[ $? -ne 0 ]]; then
@@ -14,21 +15,14 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-# Copy the files into a temp directory
-mkdir .deployed
-cp -r ./public/* .deployed/
+mkdir $DEPLOY_FOLDER
+cd $DEPLOY_FOLDER
+git clone -b gh-pages git@github.com:JRacaud/JRacaud.github.io.git gh-pages
 
-if [[ $? -ne 0 ]]; then
-    echo "[ERROR]: Cannot backup website. Aborting..."
-    read -s "Press enter to exit..."
-    exit 1
-fi
-
-# Checkout gh-pages branch and update everything
-git checkout gh-pages
+cd gh-pages
 rm -rf *
 
-cp -r ./deployed/* .
+cp -r $PUBLIC_FOLDER/* .
 
 if [[ $? -ne 0 ]]; then
     echo "[ERROR]: Cannot copy website. Aborting..."
@@ -36,9 +30,10 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+git add .
 git commit -am $message
 git push
 
-# We can now go back to the main branch and delete the temp folder
-git checkout main
-rm -rf .deployed
+cd $ROOT
+rm -rf $PUBLIC_FOLDER
+rm -rf $DEPLOY_FOLDER
